6c78ccfcc08c714836486e2ac1e7cef0426f6bb3
wangtao <wangtao@rd.netease.com>
2022-05-17 14:32:14 +0800

    add Q953

---
+++ src/main/java/leetcode/easy/Q953.java
@@ -0,0 +1,11 @@
+package leetcode.easy;
+
+public class Q953 {
+    public static void main(String[] args) {
+
+    }
+    public boolean isAlienSorted(String[] words, String order) {
+
+        return false;
+    }
+}
