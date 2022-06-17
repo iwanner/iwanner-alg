6594a4ba9bc7127fc68539a68e59b8fd2234bfe3
wangtao <wangtao@rd.netease.com>
2022-06-18 00:20:00 +0800

    feat：Q1738

---
+++ src/main/java/com/iwanner/leetcode/middle/Q1738.java
@@ -0,0 +1,46 @@
+package com.iwanner.leetcode.middle;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * TODO
+ *
+ * @author wangtao
+ * @since 2022/6/17
+ */
+public class Q1738 {
+
+    /**
+     * 1 1 1 1
+     * 1 1 1 1
+     * 1 1 1 1
+     * 1 1 1 1
+     */
+    public int kthLargestValue(int[][] matrix, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k, Comparator.reverseOrder());
+        int[][] dp = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                dp[i][j] = matrix[i][j];
+                if (j - 1 >= 0) {
+                    dp[i][j] = dp[i][j] ^ dp[i][j - 1];
+                }
+            }
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            int cur = dp[0][i];
+            for (int j = 1; j < matrix.length; j++) {
+                cur = cur ^ dp[j][i];
+                queue.add(cur);
+            }
+        }
+        while (k > 1) {
+            queue.poll();
+            k--;
+        }
+        return queue.isEmpty() ? -1 : queue.peek();
+    }
+
+}
